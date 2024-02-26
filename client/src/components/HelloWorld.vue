783a6e006ad78dc39d0952c4b306717b02e5ecc4
Thomas Eykamp <embergameslp@gmail.com>
2024-02-26 17:10:41 -0500

    update imports for shadcn

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -5,15 +5,9 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import { Button } from './ui/button'
+import { Button } from '@/components/ui/button'
 
 defineProps<{ msg: string }>()
 
 const count = ref(0)
 </script>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
