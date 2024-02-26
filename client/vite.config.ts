48ee213c3354d601e7edc2fcfe9bd8f4d7c4639e
Thomas Eykamp <embergameslp@gmail.com>
2024-02-26 16:53:39 -0500

    installed shadcdn

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+import path from "path"
+
+import tailwind from "tailwindcss"
+import autoprefixer from "autoprefixer"
+
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
