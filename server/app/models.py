f4f5eab15df2981f6e4ef5708ae0059183c28718
James Ferrarelli <ferrarellijames@gmail.com>
2024-04-03 14:01:04 -0400

    Create routes for querying hardcoded card and by ID.
    The /testcard route will provide Gem-Knight Seraphinite's card as a
    hardcoded json response
    
    The /testcard/<int:card_id> route will search the database for a card
    with the provided id and return that card's info in a json response.
    
    The rest has a lot to talk about:
    
    - YugiohCard model now does not take in a generic base Card as a key
        - This is for simplicity sake. Passing around card objects for
          queries is not the way to go.
        - Lets us simply search by id and simplifies the schema.
    
    - The other models are commented out, and I'm voting for them being left that
      way until those features are beginning development.
        - Otherwise they sit there, clutter the migrations, and add noise.
    
    - Refactored the unit test to make more concise, and update the card id
    
    - The templates/ directory is for Django frontend templates for testing
      purposes.
    
    - The migrations somehow changed shifted all the card ids.
        - 10000 is now 7938

--- server/app/models.py
+++ server/app/models.py
@@ -2,25 +2,13 @@ from django.conf import settings
 from django.db import models
 
 
-class Card(models.Model):
-    pass
-
-
-class Deck(models.Model):
-    name = models.CharField(max_length=100)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    date_created = models.DateTimeField()
-
-
-# Collection of cards in unorganized format; might contain cards from different TCGs
-class Binder(models.Model):
-    name = models.CharField(max_length=100)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    cards = models.ManyToManyField(Card)
+#class Deck(models.Model):
+#    name = models.CharField(max_length=100)
+#    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#    date_created = models.DateTimeField()
 
 
 class YugiohCard(models.Model):
-    card = models.OneToOneField(Card, on_delete=models.CASCADE)
     type = models.CharField(max_length=32)
     name = models.CharField(max_length=64)
     englishAttribute = models.CharField(max_length=32)
@@ -33,26 +21,23 @@ class YugiohCard(models.Model):
     status = models.IntegerField()
 
 
-class YugiohDeck(models.Model):
-    deck = models.OneToOneField(Deck, on_delete=models.CASCADE)
+# Collection of cards in unorganized format; might contain cards from different TCGs
+#class Binder(models.Model):
+#    name = models.CharField(max_length=100)
+#    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#    cards = models.ManyToManyField(YugiohCard)
 
 
-class YugiohDeckCard(models.Model):
-    class Type(models.TextChoices):
-        MAIN = "M"
-        EXTRA = "E"
-        SIDE = "S"
-
-    deck = models.ForeignKey(YugiohDeck, on_delete=models.CASCADE)
-    card = models.ForeignKey(YugiohCard, on_delete=models.CASCADE)
-
-
-class YugiohFormat(models.Model):
-    name = models.CharField(max_length=50)
-    legal_cards = models.ManyToManyField(YugiohCard)
-
-
-class YugiohSet(models.Model):
-    name = models.CharField(max_length=50)
-    cards = models.ManyToManyField(YugiohCard)
-    date_released = models.DateTimeField()
+#class YugiohDeck(models.Model):
+#    deck = models.OneToOneField(Deck, on_delete=models.CASCADE)
+#
+#
+#class YugiohFormat(models.Model):
+#    name = models.CharField(max_length=50)
+#    legal_cards = models.ManyToManyField(YugiohCard)
+#
+#
+#class YugiohSet(models.Model):
+#    name = models.CharField(max_length=50)
+#    cards = models.ManyToManyField(YugiohCard)
+#    date_released = models.DateTimeField()
